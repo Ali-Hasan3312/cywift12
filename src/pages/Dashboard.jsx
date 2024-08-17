ee8b4bd5a23672131a1d9d93838d7575c3697656
Ali-Hasan3312 <alihasan331229@gmail.com>
2024-08-17 15:08:42 +0500

    add bg-dark

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -74,9 +74,8 @@ function Dashboard() {
 
               {/* Left: Title */}
               <div className="mb-4 sm:mb-0">
-                <h1 className="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">Risk Management</h1>
+                <h1 className="text-2xl md:text-3xl text-gray-100 font-bold">Risk Management</h1>
               </div>
-
               {/* Right: Actions */}
               {/* <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
                
